499c59ac5a5634494a4d50d932be61c2b9125efb
noeliedubos <noelie.dubos@gmail.com>
2021-12-02 10:31:21 +0100

    i had my DefaultController

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -6,15 +6,17 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ProgramController extends AbstractController
+
+class DefaultController extends AbstractController
 {
-    /**
-     * @Route("/program/", name="program_index")
+     /**
+     * @Route("/", name="app_index")
      */
+
     public function index(): Response
     {
         return $this->render('program/index.html.twig', [
-            'website' => 'Wild Séries',
+            'default' => 'Bienvenue!',
          ]);
     }
 }
